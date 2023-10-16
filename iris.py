00bfeebff76306c9d329174ec3de2502061e887f
vicioms <vincenzomaria.schimmenti@gmail.com>
2023-10-16 14:56:38 +0200

    Stuff

--- iris.py
+++ iris.py
@@ -1,3 +1,4 @@
+import torch
 import pandas as pd
 import numpy as np
 import requests
@@ -8,6 +9,62 @@ from sklearn.metrics.pairwise import haversine_distances
 import zipfile
 import urllib
 import re
+from typing import Union, Optional
+
+# inspired by https://zenodo.org/record/8161777
+class Sequence:
+
+    def __init__(self,
+                 inter_times: Union[torch.Tensor, np.ndarray],
+                 t_start : float = 0.0,
+                 t_nll_start : Optional[float] = None,
+                 features : Optional[Union[torch.Tensor, np.ndarray]] = None):
+        self.inter_times =  torch.flatten(torch.as_tensor(inter_times))
+        if not self.inter_times.dtype in [torch.float32, torch.float64]:
+            raise ValueError(
+                f"Supported types for inter_times are torch.float32 or torch.float64"
+                "(got {self.inter_times.dtype})"
+            )
+        self.arrival_times = self.inter_times.cumsum(dim=-1)[:-1] + t_start
+        self.t_start = float(t_start)
+        self.t_end = float(self.inter_times.sum().item() + self.t_start)
+        if t_nll_start is None:
+            t_nll_start = t_start
+        self.t_nll_start = float(t_nll_start)
+        if(features is not None):
+            self.features = torch.as_tensor(features)
+            if self.features.shape[0] != self.arrival_times.shape[0]: # different sequence length!!!!
+                raise ValueError(
+                f"The length of features is different from the induced length of arrival times."
+            )
+        else:
+            self.features = None
+
+    def get_subsequence(self, start : float, end : float):
+        if start < self.t_start or end > self.t_end:
+            raise ValueError(
+                f"Error in either start or end. start must be >= {self.t_start} and end must be <= {self.t_end}"
+            )
+        mask = (self.arrival_times >= start) & (self.arrival_times <= end)
+        new_arrival_times = self.arrival_times[mask]
+        if(len(new_arrival_times) > 0):
+            # find the last gap, to add to the new inter_times
+            last_inter_time = torch.tensor(
+                [end - new_arrival_times[-1]],
+                device=self.inter_times.device,
+                dtype=self.inter_times.dtype,
+            )
+            # we skip the last element from inter_times
+            new_inter_times = torch.cat([self.inter_times[:-1][mask], last_inter_time])
+            # 
+            first_inter_time = new_arrival_times[0] - start
+    @staticmethod
+    def compute_inter_times(arrival_times : Union[torch.tensor, np.ndarray, list],
+                             t_start : float, 
+                             t_end : float):
+        return np.diff(arrival_times, prepend=[t_start], append=[t_end])
+    
+
 
 class irisRequests:
     stations_base_url = "http://service.iris.edu/fdsnws/station/1/query?"
@@ -195,3 +252,13 @@ class irisRequests:
         mag_f = irisRequests.__format_magnitude(minmag,maxmag,  magtype)
         s = irisRequests.url_events([time_f, box_f, mag_f]) + "format=" + format
         return s
+    
+    @staticmethod
+    def retrieve_events_box(start_time, end_time, minlat, maxlat, minlon, maxlon, minmag=None, maxmag=None, magtype=None):
+        download_url = irisRequests.url_events_box(start_time, end_time, minlat, maxlat, minlon, maxlon, minmag, maxmag, magtype)
+        df = pd.read_csv(download_url, sep="|", comment="#")
+        df.Time = pd.to_datetime(df.Time, errors='coerce')
+        df.dropna(axis=0, inplace=True)
+        df.sort_values(by="Time", inplace=True)
+        df.reset_index(inplace=True, drop=True)
+        return df
